bef4f258fecf0a8e473b6a73cf10186a6c912891
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-03 16:39:23 +0530

    feat: Add MenuBar

---
+++ frontend/src/features/ui/sidebar/MenuBar.tsx
@@ -0,0 +1,20 @@
+export interface MenuBarProps extends React.HTMLAttributes<HTMLElement> {}
+
+function MenuBar({
+  className = "",
+  children,
+  ref,
+  ...props
+}: MenuBarProps & { ref?: React.Ref<HTMLElement> }) {
+  return (
+    <nav
+      ref={ref}
+      className={`flex flex-col gap-1 p-2 ${className}`}
+      {...props}
+    >
+      {children}
+    </nav>
+  );
+}
+
+export default MenuBar;
