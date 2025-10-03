80e71fd5af3fb21ca71b89762605a1af92c33657
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-03 16:34:55 +0530

    feat: Add routing to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
+import { Outlet } from "react-router";
+import SideBar from "./features/ui/sidebar/Sidebar";
+
+const navItems = [
+  { to: "/", label: "Dashboard", end: true },
+  { to: "/myproject", label: "My Project" },
+];
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex h-screen">
+        <SideBar navItems={navItems} />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
