bbe481030c1909b16a267d1bc7167f2b03481c78
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-04 16:46:58 +0530

    feat: Add CardContent.tsx

---
+++ frontend/src/features/ui/card/CardContent.tsx
@@ -0,0 +1,18 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cn } from "../../../lib/utils";
+
+export interface CardContentProps extends React.HTMLAttributes<HTMLDivElement> {
+  asChild?: boolean;
+}
+
+function CardContent({
+  className = "",
+  asChild = false,
+  ref,
+  ...props
+}: CardContentProps & { ref?: React.Ref<HTMLDivElement> }) {
+  const Comp = asChild ? Slot : "div";
+  return <Comp className={cn("p-6 pt-0", className)} ref={ref} {...props} />;
+}
+
+export default CardContent;
