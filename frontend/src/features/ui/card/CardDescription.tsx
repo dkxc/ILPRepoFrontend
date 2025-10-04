7d7918f7c595a0abf66fd1f59664cba28e527102
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-04 16:47:09 +0530

    feat: Add CardDescription.tsx

---
+++ frontend/src/features/ui/card/CardDescription.tsx
@@ -0,0 +1,19 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cn } from "../../../lib/utils";
+
+export interface CardDescriptionProps
+  extends React.HTMLAttributes<HTMLParagraphElement> {
+  asChild?: boolean;
+}
+
+function CardDescription({
+  className = "",
+  asChild = false,
+  ref,
+  ...props
+}: CardDescriptionProps & { ref?: React.Ref<HTMLParagraphElement> }) {
+  const Comp = asChild ? Slot : "p";
+  return <Comp className={cn("text-sm", className)} ref={ref} {...props} />;
+}
+
+export default CardDescription;
