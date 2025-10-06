c84454164c183bad9f2819618f20677297152cdf
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-06 10:24:19 +0530

    refactor: Set example for ProjectCard

--- frontend/src/pages/trainee/Dashboard.tsx
+++ frontend/src/pages/trainee/Dashboard.tsx
@@ -1,16 +1,31 @@
 import ProjectCard from "../../features/trainee/dashboard/ProjectCard";
+import type { Project } from "../../features/trainee/types/Project.types";
 
 function Dashboard() {
+  const project: Project = {
+    id: 21231,
+    title: "ILP Repo",
+    status: "Live",
+    technologies: [".NET", "React"],
+    team: {
+      number: 4,
+      members: [
+        "Dhanush Kovi",
+        "Nino Jagadish",
+        "Raihana Rasaldeen",
+        "Merlin Baiju",
+        "Nandhu Krishna",
+        "Mohammed Aiman",
+        "Alex Joseph Pius",
+      ]
+    },
+    progress: 60
+  }
+
   return (
     <>
       <div className="grid gap-4 p-4 md:grid-cols-2 lg:grid-cols-4">
-        <ProjectCard className="col-span-2 bg-sidebar-and-header-background"></ProjectCard>
-        <ProjectCard className="bg-sidebar-and-header-background"></ProjectCard>
-        <ProjectCard className="bg-sidebar-and-header-background"></ProjectCard>
-        <ProjectCard className="bg-sidebar-and-header-background"></ProjectCard>
-        <ProjectCard className="bg-sidebar-and-header-background"></ProjectCard>
-        <ProjectCard className="bg-sidebar-and-header-background"></ProjectCard>
-        <ProjectCard className="bg-sidebar-and-header-background"></ProjectCard>
+        <ProjectCard project={project} />
       </div>
     </>
   );
