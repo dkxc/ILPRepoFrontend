4bd2840693d7ee081210884f8191c65d71acf96f
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-07 14:28:53 +0530

    fix: Remove mantine styles temporarily

--- frontend/src/pages/admin/Projects.tsx
+++ frontend/src/pages/admin/Projects.tsx
@@ -4,7 +4,6 @@ import { modals } from "@mantine/modals";
 import { notifications } from "@mantine/notifications";
 import { Trash2, Users, RefreshCw, CheckCircle, Clock } from "lucide-react";
 import DataTable, { type ColumnDef } from "../../features/ui/Table";
-import "@mantine/core/styles.css";
 import Button from "../../features/ui/Button";
 // import DataTable, { type ColumnDef } from './DataTable';
 
