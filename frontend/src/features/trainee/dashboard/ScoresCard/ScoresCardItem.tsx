3e5fae833f25dccfa35e12aaabc3e6e2b92a4454
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-08 10:35:02 +0530

    feat: Add ScoreCardItem

---
+++ frontend/src/features/trainee/dashboard/ScoresCard/ScoresCardItem.tsx
@@ -0,0 +1,22 @@
+import { cn } from "../../../../lib/utils";
+import type { ScoreItem } from "../../types/scores/ScoreItem.types";
+
+export interface ScoreCardItemProps extends React.HTMLAttributes<HTMLDivElement> {
+    item: ScoreItem
+}
+
+function ScoreCardItem({
+    item,
+    className,
+    ref,
+    ...props
+}: ScoreCardItemProps & { ref?: React.Ref<HTMLDivElement> }) {
+    return (
+        <div className={cn("flex flex-col items-center-safe justify-between text-center", className)} ref={ref} {...props}>
+            <div><h2 className="font-medium text-2xl">{item.value}</h2></div>
+            <div className="text-sm overflow-hidden">{item.caption}</div>
+        </div>
+    )
+}
+
+export default ScoreCardItem;
