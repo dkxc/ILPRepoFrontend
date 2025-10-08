562fab5e55d6904e0f3ac561a6aa45f78c030181
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-08 10:35:57 +0530

    feat: Add ScoresCard

--- frontend/src/features/trainee/dashboard/ScoresCard/index.tsx
+++ frontend/src/features/trainee/dashboard/ScoresCard/index.tsx
@@ -1,15 +1,16 @@
-import { type Project } from "../types/Project.types";
-import * as Card from "../../ui/card";
-import Button from "../../ui/Button";
+import { type Scores } from "../../types/scores/Score.types";
+import * as Card from "../../../ui/card";
+import Button from "../../../ui/Button";
 
-import { cn } from "../../../lib/utils";
+import { cn } from "../../../../lib/utils";
+import ScoreCardItem from "./ScoresCardItem";
 
 export interface ScoreCardProps extends React.HTMLAttributes<HTMLDivElement> {
-  project: Project;
+  scores: Scores;
 }
 
 function ScoreCard({
-  project,
+  scores,
   className,
   ref,
   ...props
@@ -23,23 +24,37 @@ function ScoreCard({
       ref={ref}
       {...props}
     >
-      <div>
+      <div className="w-full">
         <Card.CardHeader>
           <Card.CardTitle>Scores</Card.CardTitle>
         </Card.CardHeader>
 
-        <Card.CardContent></Card.CardContent>
-
-        <Card.CardFooter>
+        <Card.CardContent>
           <Button
             size="sm"
             className="rounded-2xl px-4 bg-red-700 hover:bg-red-900"
           >
             Under Construction
           </Button>
+        </Card.CardContent>
+
+        <Card.CardFooter>
+          <div>
+
+          </div>
+          <div className="flex gap-4">
+            {
+              scores.courses.map((scoreItem) => (
+                <div key={scoreItem.caption} className="flex-1">
+                <>
+                  <ScoreCardItem item={scoreItem} />
+                </>
+                </div>
+              ))
+            }
+          </div>
         </Card.CardFooter>
       </div>
-      <div className="flex-1">{/* Pie Chart */}</div>
     </Card.Card>
   );
 }
