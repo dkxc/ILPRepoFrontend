b0f7bae0adbd50a40951ec90b7d1ecd09567b0a1
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-11 16:55:06 +0530

    style: Decrease DocumentsCard size

--- frontend/src/features/trainee/dashboard/DocumentsCard/index.tsx
+++ frontend/src/features/trainee/dashboard/DocumentsCard/index.tsx
@@ -30,7 +30,7 @@ function DocumentsCard({
           <Card.CardTitle>Documents</Card.CardTitle>
         </Card.CardHeader>
 
-        <Card.CardContent className="flex flex-col h-64 max-h-64 overflow-y-auto">
+        <Card.CardContent className="flex flex-col h-48 max-h-48 overflow-y-auto">
           {documents.map((doc) => (
             <NavLink
               key={doc.url}
