f3cb9829a736d5b32a0128d164854e1ff9803a4e
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-14 15:08:48 +0530

    feat: Add Graphs & Edit btn to ProjectCard

--- frontend/src/features/trainee/dashboard/ProjectCard.tsx
+++ frontend/src/features/trainee/dashboard/ProjectCard.tsx
@@ -3,10 +3,26 @@ import * as Card from "../../ui/card";
 import Button from "../../ui/Button";
 
 import { cn } from "../../../lib/utils";
+import { ResponsivePie } from "@nivo/pie";
+import { getPieDataFromPercent } from "../../../lib/graphs/utils";
+import { Pencil, Radio, UploadCloud } from "lucide-react";
+import Badge from "../../ui/badges/Badge";
 
 export interface ProjectCardProps extends React.HTMLAttributes<HTMLDivElement> {
   project: Project;
 }
+
+const getProgressBadgeVariant = (
+  status: "Live" | "In Progress" | "Completed",
+) => {
+  if (status === "Live") {
+    return "success";
+  } else if (status === "In Progress") {
+    return "warn";
+  } else {
+    return "none";
+  }
+};
 
 function ProjectCard({
   project,
@@ -23,27 +39,110 @@ function ProjectCard({
       ref={ref}
       {...props}
     >
-      <div>
-        <Card.CardHeader>
-          <Card.CardTitle>{project.title}</Card.CardTitle>
-          <Card.CardDescription>
-            Team {project.team.number}
-          </Card.CardDescription>
-        </Card.CardHeader>
+      <div className="flex justify-between w-full">
+        <div className="self-center-safe">
+          <Card.CardHeader>
+            <Card.CardTitle>{project.title}</Card.CardTitle>
+            <Card.CardDescription>
+              Team {project.team.number}
+            </Card.CardDescription>
+          </Card.CardHeader>
 
-        <Card.CardContent className="flex flex-col gap-1 text-sm">
-          <div>Status: {project.status}</div>
-          <div>Technology Used: </div>
-          <div>No. of Team Members: {project.team.members.length}</div>
-        </Card.CardContent>
+          <Card.CardContent className="flex flex-col gap-2 text-sm">
+            <div className="flex items-center-safe gap-1">
+              <span>Status:</span>
+              <Badge variant={getProgressBadgeVariant(project.status)}>
+                <Radio />
+                {project.status}
+              </Badge>
+            </div>
+            <div className="flex items-center-safe gap-1">
+              <span>Technology Used:</span>
+              {project.technologies.map((tech) => (
+                <Badge variant="none" key={tech}>
+                  {tech}
+                </Badge>
+              ))}
+            </div>
+            <div>
+              No. of Team Members:{" "}
+              <span className="font-semibold">
+                {project.team.members.length}
+              </span>
+            </div>
+          </Card.CardContent>
 
-        <Card.CardFooter>
-          <Button size="sm" className="rounded-2xl px-4">
-            Upload Documents
-          </Button>
-        </Card.CardFooter>
+          <Card.CardFooter className="flex gap-2">
+            <Button size="sm" className="rounded-2xl px-4">
+              <UploadCloud /> Upload Documents
+            </Button>
+            <Button
+              size="sm"
+              className="rounded-2xl px-4 text-text-base bg-gray-200 hover:bg-gray-300"
+            >
+              <Pencil /> Edit Project Details
+            </Button>
+          </Card.CardFooter>
+        </div>
+        <div className="w-48 m-4">
+          <ResponsivePie
+            data={getPieDataFromPercent(project.progress, {
+              1: "Uploaded",
+              2: "Not Uploaded",
+            })}
+            colors={["var(--color-brand-500)", "var(--color-inactive-badge)"]}
+            innerRadius={0.6}
+            padAngle={0.6}
+            cornerRadius={2}
+            activeOuterRadiusOffset={8}
+            enableArcLinkLabels={false}
+            enableArcLabels={false}
+            margin={{ top: 8, right: 8, bottom: 8, left: 8 }}
+            layers={[
+              "arcs",
+              ({ centerX, centerY }) => (
+                <>
+                  <text
+                    x={centerX}
+                    y={centerY}
+                    dy={-10}
+                    textAnchor="middle"
+                    dominantBaseline="central"
+                    className="text-2xl font-semibold text-text-base"
+                  >
+                    {`${project.progress}%`}
+                  </text>
+                  <text
+                    x={centerX}
+                    y={centerY}
+                    dy={14}
+                    textAnchor="middle"
+                    dominantBaseline="central"
+                    className="text-xs text-text-base"
+                  >
+                    Uploaded
+                  </text>
+                </>
+              ),
+            ]}
+            tooltip={({ datum }) => (
+              <div className="p-2 bg-white rounded-md shadow-md text-sm font-secondary whitespace-nowrap">
+                <span
+                  className={cn(
+                    "font-medium",
+                    (datum.color === "var(--color-brand-500)" &&
+                      "text-brand") ||
+                      "text-text-error",
+                  )}
+                >
+                  {datum.label}
+                </span>
+                : {datum.value}%
+              </div>
+            )}
+          />
+        </div>
       </div>
-      <div className="flex-1">{/* Pie Chart */}</div>
     </Card.Card>
   );
 }
