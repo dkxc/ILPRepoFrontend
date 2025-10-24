e635d2e7a74d8d463d2f9aaba4e7c354af23f599
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-24 03:07:24 +0000

    feat: Set up vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,27 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouter } from "@react-router/dev/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), !process.env.VITEST && reactRouter()],
+  test: {
+    environment: "happy-dom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      reportOnFailure: true,
+      include: ["src/**/*.{ts,tsx,js,jsx}"],
+      exclude: [
+        "**/node_modules/**",
+        "**/dist/**",
+        "**/src/test/**",
+        "**/src/mocks/**",
+        "**/*.types.ts",
+        "**/*.config.ts",
+      ],
+    },
+  },
   optimizeDeps: {
     include: ["@mantine/core", "@mantine/hooks"],
   },
