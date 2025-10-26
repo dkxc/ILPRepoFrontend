2eb5d2f6525b537a6a5d66cf4f8018bf51805647
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-26 01:03:43 +0000

    feat: Simplify paths using absolute paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouter } from "@react-router/dev/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), !process.env.VITEST && reactRouter()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@ui": path.resolve(__dirname, "./src/ui"),
+    },
+  },
   test: {
     environment: "happy-dom",
     setupFiles: "./src/test/setup.ts",
@@ -19,7 +27,7 @@ export default defineConfig({
         "**/src/mocks/**",
         "**/*.types.ts",
         "**/*.config.ts",
-        '**/index.ts',
+        "**/index.ts",
       ],
     },
   },
