f3da0dfd912878c91aa4aadee56027a8160edc3a
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-27 11:29:22 +0530

    refactor: Remove redundant import in BatchCardSuccess

--- frontend/src/features/trainee/dashboard/components/BatchCard/components/BatchCardSuccess.tsx
+++ frontend/src/features/trainee/dashboard/components/BatchCard/components/BatchCardSuccess.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { type Batch } from "../../../../types/Batch.types";
 import { getStatusBadgeVariant } from "../utils/Batch.utils";
 import * as Card from "../../../../../ui/card";
