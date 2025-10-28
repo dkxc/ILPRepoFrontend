8265729b4e309c22cf9ce32fa76b6a3c61b0eb7a
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-28 17:53:10 +0000

    build: Add deps for Untitled UI

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * This function does nothing besides helping us to be able to
+ * sort the classes inside style objects which is not supported
+ * by the Tailwind IntelliSense by default.
+ */
+export function sortCx<
+  T extends Record<
+    string,
+    | string
+    | number
+    | Record<string, string | number | Record<string, string | number>>
+  >,
+>(classes: T): T {
+  return classes;
+}
