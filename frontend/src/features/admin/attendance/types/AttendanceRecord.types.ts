b2db989262541be142ef544768d51a4a78a3914b
dkxc <224724887+dkxc@users.noreply.github.com>
2025-10-31 16:12:54 +0530

    feat: Separate types for Attendance Queries & Records

--- frontend/src/features/admin/attendance/types/AttendanceRecord.types.ts
+++ frontend/src/features/admin/attendance/types/AttendanceRecord.types.ts
@@ -1,3 +1,5 @@
+import type { GetResponseType } from "./AttendanceQuery.types";
+
 export const AttendanceStatus = {
   Present: "P",
   Absent: "A",
@@ -7,43 +9,9 @@ export const AttendanceStatus = {
 export type AttendanceStatus =
   (typeof AttendanceStatus)[keyof typeof AttendanceStatus];
 
-// For GET: /api/attendance/batch/{batchId}
-export type GetQueryType = {
-  start_date?: string;
-  end_date?: string;
-};
-
-export type GetResponseType = {
-  traineeId: number;
-  traineeName: string;
-  dates: {
-    [date: string]: {
-      forenoon: AttendanceStatus;
-      afternoon: AttendanceStatus;
-    };
-  };
-};
-
-// For PUT: /api/attendance/batch/{batchId}
-export type BulkUpdateStatus = Partial<{
-  forenoon: AttendanceStatus;
-  afternoon: AttendanceStatus;
-}>;
-
-export type UpdateQueryType = {
-  traineeIds: number[];
-  startDate: string;
-  endDate: string;
-  status: BulkUpdateStatus;
-};
-
-export type UpdateSuccessResponseType = {
-  status: "success";
-  updateRecordCount: number;
-};
-
-// API Errors
-export type ApiErrorResponse = {
-  statusCode: number;
-  message: string;
+export type ProcessedTraineeData = GetResponseType & {
+  presentFN: number;
+  presentAN: number;
+  totalPresentDays: number;
+  totalPossibleDays: number;
 };
